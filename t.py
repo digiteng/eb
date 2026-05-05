e62df9f8ae6cde71c9da60d60e3c05c3c0a5de2e
digiteng <digiteng@gmail.com>
2026-05-05 22:30:54 +0300

    Update t.py

--- t.py
+++ t.py
@@ -5,9 +5,11 @@ import os
 
 # Retrieve the environment variable
 api_key = os.environ.get('API_KEY')
+api_keyv = os.environ.get('API_KEYV')
 print(api_key)
-if api_key:
-	print("API Key successfully loaded!")
-	# Use your key here
-else:
-	print("Error: API Key not found.")
+print(api_keyv)
+# if api_key:
+	# print("API Key successfully loaded!")
+	Use your key here
+# else:
+	# print("Error: API Key not found.")
