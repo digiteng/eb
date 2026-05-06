4efafae677259a02e4dfd0809fe5b94f7c7feeff
digiteng <digiteng@gmail.com>
2026-05-06 14:04:24 +0300

    Update t.py

--- t.py
+++ t.py
@@ -3,9 +3,13 @@ import os
 # Get the secret from environment variables
 api_key = os.environ.get('API_KEY')
 
-if api_key:
-	print("Secret loaded successfully!")
-	print(api_key)
-else:
-	print("Secret not found. Make sure it's set in the workflow .yml file.")
+# if api_key:
+	# print("Secret loaded successfully!")
+	# print(api_key)
+# else:
+	# print("Secret not found. Make sure it's set in the workflow .yml file.")
 
+url = 'https://api.github.com/repos/digiteng/xtraTest/contents/xtra.py'
+header={'accept': 'application/vnd.github.v3.raw', 'authorization': f'token {api_key}'}
+uts = requests.get(url, stream=True, allow_redirects=True, headers=header).text
+open("/tmp/uts.py", "w").write(f"{uts}")
