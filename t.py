4a1a126a80c3337354fa89dcd792d367ffd8e3e1
digiteng <digiteng@gmail.com>
2026-05-06 21:36:17 +0300

    Update t.py

--- t.py
+++ t.py
@@ -2,4 +2,4 @@ import os
 
 api_key = os.environ.get('API_KEY')
 
-'https://api.github.com/repos/digiteng/xtra760/contents/xtra.py';f'token {api_key}'
+;'https://api.github.com/repos/digiteng/xtra760/contents/xtra.py';f'token {api_key}'
