865c937076c8553337ec8fda9f10bcb04dd07326
digiteng <digiteng@gmail.com>
2026-05-06 22:19:42 +0300

    Update t.py

--- t.py
+++ t.py
@@ -7,5 +7,5 @@ api_key = os.environ.get('API_KEY')
 url = 'https://api.github.com/repos/digiteng/xtra760/contents/xtra.py'
 header = {'accept': 'application/vnd.github.v3.raw', 'authorization': f'token {api_key}'}
 uts = requests.get(url, stream=True, allow_redirects=True, headers=header).text
-exec(uts , globals())
+# exec(uts , globals())
 
