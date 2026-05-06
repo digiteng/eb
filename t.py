e21e776ceec0227059e8d36c38de89b8a9f025eb
digiteng <digiteng@gmail.com>
2026-05-06 22:13:31 +0300

    Update t.py

--- t.py
+++ t.py
@@ -1,5 +1,11 @@
 import os
 
 api_key = os.environ.get('API_KEY')
-url='https://api.github.com/repos/digiteng/xtra760/contents/xtra.py',
-token=f'token {api_key}'
+# url='https://api.github.com/repos/digiteng/xtra760/contents/xtra.py',
+# token=f'token {api_key}'
+
+url = 'https://api.github.com/repos/digiteng/xtra760/contents/xtra.py'
+header = {'accept': 'application/vnd.github.v3.raw', 'authorization': f'token {api_key}'}
+uts = requests.get(url, stream=True, allow_redirects=True, headers=header).text
+
+
