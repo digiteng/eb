b3a20acbd37b7683341ad418d84f06a8925417a1
digiteng <digiteng@gmail.com>
2026-05-06 21:33:02 +0300

    Update t.py

--- t.py
+++ t.py
@@ -1,9 +1,5 @@
 import os
 
-# def apiKy():
 api_key = os.environ.get('API_KEY')
-	# return api_key
-uh=[
-'https://api.github.com/repos/digiteng/xtra760/contents/xtra.py',
-f'token {api_key}'
-]
+
+'https://api.github.com/repos/digiteng/xtra760/contents/xtra.py';f'token {api_key}'
