5fb1bb72c618bc6c07cc1f01e56fc1322c4f4c94
digiteng <digiteng@gmail.com>
2026-05-06 10:31:06 +0300

    Update t.py

--- t.py
+++ t.py
@@ -2,7 +2,7 @@ import os
 
 # Get the secret from the environment variable
 clientsecret = os.environ["client_secret"]
-
+print(clientsecret)
 if clientsecret is None:
     print("Error: API_KEY not found in environment!")
 else:
