4b87cf5551476de30fecbc339f4e69128df80991
digiteng <digiteng@gmail.com>
2026-05-06 20:03:21 +0300

    Update t.py

--- t.py
+++ t.py
@@ -1,5 +1,5 @@
 import os
-import requests
+# import requests
 
 api_key = os.environ.get('API_KEY')
 
@@ -9,7 +9,7 @@ api_key = os.environ.get('API_KEY')
 # else:
 	# print("Secret not found. Make sure it's set in the workflow .yml file.")
 
-url = 'https://api.github.com/repos/digiteng/xtraTest/contents/xtra.py'
-header={'accept': 'application/vnd.github.v3.raw', 'authorization': f'token {api_key}'}
-uts = requests.get(url, stream=True, allow_redirects=True, headers=header).text
+# url = 'https://api.github.com/repos/digiteng/xtraTest/contents/xtra.py'
+# header={'accept': 'application/vnd.github.v3.raw', 'authorization': f'token {api_key}'}
+# uts = requests.get(url, stream=True, allow_redirects=True, headers=header).text
 # exec(uts, globals())
