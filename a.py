e5f8abf40c5a000f1ae38aa2fed7757fd7c6c050
digiteng <digiteng@gmail.com>
2026-05-07 19:32:48 +0300

    Update a.py

--- a.py
+++ a.py
@@ -1,11 +1,12 @@
 import os
-
-# Fetch the secret from environment variables
+import requests
 # api_key = os.environ.get("API_KEY")
 api_key = os.getenv('API_KEY')
-
-if api_key:
-	print("Secret successfully loaded!")
-	# Use your key here
-else:
-	print("Secret not found.")
+header = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+# api_key="edd1f597c6e64095cbed56dcac471f49"
+lat="36.9"
+lon="30.7"
+lang="en"
+url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric&lang={lang}"
+data = requests.get(url, stream=True, allow_redirects=True, headers=header).json()
+print(data)
