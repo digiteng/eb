186a3e230ca124ca88a07d07fbc4d664a2b1862e
digiteng <digiteng@gmail.com>
2026-05-07 19:01:31 +0300

    Update a.py

--- a.py
+++ a.py
@@ -1,7 +1,8 @@
 import os
 
 # Fetch the secret from environment variables
-api_key = os.environ.get("API_KEY")
+# api_key = os.environ.get("API_KEY")
+api_key = os.getenv('API_KEY')
 
 if api_key:
 	print("Secret successfully loaded!")
