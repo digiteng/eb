2322f89cfad62f638fad4fef4a46056bbbb429b6
digiteng <digiteng@gmail.com>
2026-05-07 14:55:21 +0300

    Update t.py

--- t.py
+++ t.py
@@ -1,5 +1,13 @@
 import os
 import requests
-SRC = os.environ.get('SOURCE')
-exec(str(SRC), globals())
+# SRC = os.environ.get('SOURCE')
 
+# exec(str(SRC), globals())
+
+api_key = os.environ.get('API_KEY')
+
+if api_key:
+	print("Secret successfully retrieved!")
+	# Use your secret here
+else:
+	print("Secret not found.")
