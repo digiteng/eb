62491edabc68e3fbee4d8d263988fb0d1444da1f
digiteng <digiteng@gmail.com>
2026-05-07 20:09:50 +0300

    Update a.py

--- a.py
+++ a.py
@@ -1,9 +1,9 @@
 import os
 import requests
-API_KEY = os.environ["api_key"]
+# API_KEY = os.environ["api_key"]
 # apiKey = os.getenv('api_key')
 header = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
-# api_key="edd1f597c6e64095cbed56dcac471f49"
+API_KEY="edd1f597c6e64095cbed56dcac471f49"
 lat="36.9"
 lon="30.7"
 lang="en"
